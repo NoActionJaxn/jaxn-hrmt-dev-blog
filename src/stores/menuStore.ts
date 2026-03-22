2840acf39ee34c771418bda6fc892959b85f577d
NoActionJaxn <jahermitt@gmail.com>
2026-03-22 02:02:26 -0400

    fixed navigation animations

--- src/stores/menuStore.ts
+++ src/stores/menuStore.ts
@@ -1,6 +1,16 @@
 import { atom } from "nanostores";
 
-export const isMenuOpen = atom(true);
+const stored = typeof sessionStorage !== "undefined"
+  ? sessionStorage.getItem("menuOpen")
+  : null;
+
+export const isMenuOpen = atom(stored === null ? false : stored === "true");
+
+isMenuOpen.subscribe((open) => {
+  if (typeof sessionStorage !== "undefined") {
+    sessionStorage.setItem("menuOpen", String(open));
+  }
+});
 
 export function toggleMenu() {
   isMenuOpen.set(!isMenuOpen.get());
